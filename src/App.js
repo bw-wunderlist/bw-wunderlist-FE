612028f18e78b3dc0789f0275631694230b10406
Anthony Johnson <lambda_school_loaner_82@Lambda-School-Loaner-82s-MacBook-Pro.local>
2019-04-17 21:52:11 -0400

    ..... error update .....

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import Edit from '../src/Components/UserProfile/Edit'
 // import connect  from 'react-redux';
 
 import  RegisterPage   from './Components/RegisterPage/RegisterPage';
 import Login from './Components/LoginPage/login'
 import  TodoApp from './Components/Todo/TodoApp'
-import Edit from '../src/Components/UserProfile/Edit'
 
-import './App.css';
+
+
 
 
 const App = () => {
