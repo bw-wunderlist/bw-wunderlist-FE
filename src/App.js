0e1ac8c59831f293afedf487d151992b8439d7dc
Maksim <maks112v@gmail.com>
2019-04-17 22:00:51 -0400

    asdfsadf

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@ import React from "react";
 import { Route } from "react-router-dom";
 // import connect  from 'react-redux';
 
-import  RegisterPage   from './Components/RegisterPage/RegisterPage';
-import Login from './Components/LoginPage/login'
-import  TodoApp from './Components/Todo/TodoApp'
-import Edit from './Components/UserProfile/Edit'
-import './App.css';
-
+import RegisterPage from "./Components/RegisterPage/RegisterPage";
+import Login from "./Components/LoginPage/login";
+import TodoApp from "./Components/Todo/TodoApp";
+//import Edit from "./Components/UserProfile/Edit";
+import "./App.css";
 
 const App = () => {
-  
-    return (
-      <div className="App">
-       
-           <div> 
-              <Route exact path='/' render={props => <Login {...props} /> }/>
-             <Route path='/register' render={props => <RegisterPage {...props} /> }/>
-             <Route path='/todo' render={props => <TodoApp {...props} /> } />
-             
-          </div>
-         
-
+  return (
+    <div className="App">
+      <div>
+        <Route exact path="/" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <RegisterPage {...props} />} />
+        <Route path="/todo" render={props => <TodoApp {...props} />} />
       </div>
     </div>
   );
