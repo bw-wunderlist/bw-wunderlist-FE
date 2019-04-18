522b3907451e8a6bcb01ac2df5b9799dc52a37fc
Alexnadria Supriana <alexa9410@gmail.com>
2019-04-17 20:35:22 -0400

    Update userProfile.js

--- src/Components/UserProfile/userProfile.js
+++ src/Components/UserProfile/userProfile.js
@@ -1,18 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 class UserProfile extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      userdata: {
-        username: "",
-        email: "",
-        imageurl: ""
-      }
-    };
-  }
+    constructor(props) {
+        super()
+        this.state= {
+            userdata: {
+                username: '',
+                email: '',
+                imageurl: ''
+            }
+        }
+    }
 
   render() {
     const { user } = this.props;
@@ -20,8 +20,12 @@ class UserProfile extends React.Component {
     return (
       <div className="card profile">
         <div className="card-block">
-          <img className="img-circle avatar" src={user.data.avatar} />
-          <h4 className="card-title">Hi {user.data.nickname}!</h4>
+          <img
+            className="img-circle avatar"
+            src={user.data.avatar} />
+          <h4 className="card-title">
+            Hi {user.data.nickname}!
+          </h4>
           <div className="card-text">
             <p>You have permission to perform the following:</p>
             <ul className="permissions">
@@ -38,6 +42,7 @@ class UserProfile extends React.Component {
       </div>
     );
   }
+
 }
 
 UserProfile.propTypes = {
@@ -48,10 +53,10 @@ UserProfile.defaultProps = {
   user: {
     id: 1,
     data: {
-      username: "ayla",
-      avatar:
-        "https://cloud.githubusercontent.com/assets/2637399/19027069/a356e82a-88e1-11e6-87d8-e3e74f55c069.png"
+      username: 'ayla',
+      avatar: 'https://cloud.githubusercontent.com/assets/2637399/19027069/a356e82a-88e1-11e6-87d8-e3e74f55c069.png'
     }
   }
 };
+
 export default UserProfile;
